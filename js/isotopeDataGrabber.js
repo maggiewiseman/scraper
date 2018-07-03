96637f6e384fbab41d9c4831bfb93d0f4d870ea2
Maggie Wiseman <maggie.wiseman@gmail.com>
2018-07-03 17:36:07 -0400

    got it mostly working, need to implement counter

--- js/isotopeDataGrabber.js
+++ js/isotopeDataGrabber.js
@@ -1,16 +1,18 @@
 //start with element list
 
 function IsotopeGrabber() {
-  var elementString = "hydrogen|helium|lithium|Beryllium|Boron|Carbon|nitrogen|Nitrogen|oxygen|fluorine|neon|sodium|magnesium|aluminium|silicon|phosphorus|sulfur|chlorine|argon|potassium|calcium|scandium|titanium|vanadium|chromium|manganese|iron|cobalt|nickel|copper|zinc|gallium|germanium|arsenic|selenium|bromine|krypton|rubidium|strontium|yttrium|zirconium|niobium|molybdenum|technetium|ruthenium|rhodium|Palladium|silver|Cadmium|indium|tin|antimony|tellurium|iodine|xenon|Caesium|Barium|lanthanum|cerium|praseodymium|neodymium|promethium|samarium|europium|gadolinium|terbium|dysprosium|holmium|erbium|thulium|ytterbium|lutetium|hafnium|tantalum|tungsten|rhenium|osmium|iridium|platinum|gold|mercury|thallium|lead|Bismuth|polonium|astatine|radon|francium|radium|actinium|thorium|protactinium|uranium|neptunium|Plutonium|plutonium|americium|curium|Berkelium|Californium|einsteinium|fermium|mendelevium|nobelium|dubnium|seaborgium|Bohrium|rutherfordium|hassium|meitnerium|darmstadtium|copernicium|nihonium|flerovium|oganesson"
+  var isotopeArray = [["elementName", "atomicNumber", "neutrons", "mass", "halfLife", "massNumber", "isotopeName"]];
+  var elementString = "hydrogen|helium|lithium|Beryllium|Boron|Carbon|nitrogen|Nitrogen|oxygen|fluorine|neon|sodium|magnesium|aluminium|silicon|phosphorus|sulfur|chlorine|argon|potassium|calcium|scandium|titanium|vanadium|chromium|manganese|iron|cobalt|nickel|copper|zinc|gallium|germanium|arsenic|selenium|bromine|krypton|rubidium|strontium|yttrium|zirconium|niobium|molybdenum|technetium|ruthenium|rhodium|Palladium|silver|Cadmium|indium|tin|antimony|tellurium|iodine|xenon|Caesium|Barium|lanthanum|cerium|praseodymium|neodymium|promethium|samarium|europium|gadolinium|terbium|dysprosium|holmium|erbium|thulium|ytterbium|lutetium|hafnium|tantalum|tungsten|rhenium|osmium|iridium|platinum|gold|mercury|thallium|lead|Bismuth|polonium|astatine|radon|francium|radium|actinium|thorium|protactinium|uranium|neptunium|Plutonium|plutonium|americium|curium|Berkelium|Californium|einsteinium|fermium|mendelevium|nobelium|dubnium|seaborgium|Bohrium|rutherfordium|hassium|meitnerium|darmstadtium|copernicium|nihonium|flerovium|oganesson";
 
   var elementArr = elementString.split('|');
+  //var elementArr = ['boron', 'helium'];
 
   this.go = function() {
     //looop through the element array.
     elementArr.forEach(element => {
       //create the url, call Json and give callback
       let url = `http://en.wikipedia.org/w/api.php?action=parse&page=Isotopes_of_${element.toLowerCase()}&prop=text&format=json&callback=?`
-      getJson(url, makeHtml, element);
+      getJson(url, makeHtml, element.toLowerCase());
     });
   }
 
@@ -23,16 +25,67 @@ function IsotopeGrabber() {
 
 
   function makeHtml(wikiHtml, elementName) {
-    $('#elementData').append(`<div class=${elementName}>${wikiHtml}</div>`);
+    $('#isotopeData').append(`<div id=${elementName}>${wikiHtml}</div>`);
     console.log()
     parseIsotopeData(elementName);
+    addAdditionalColumns();
+    let csv = convertArrayToCsv(isotopeArray);
+    downloadCsv(csv, "isotopeData.csv");
 
   }
 
   function parseIsotopeData(elementName) {
     console.log(elementName);
+    let table  = $(`#isotopeData > div#${elementName} #List_of_isotopes`).parent().next();
+    let htmlRows = table.children().children();
+
+    for (var i = 1; i < htmlRows.length; i++) {
+      //loop through children
+
+      var row = htmlRows[i].children;
+      if(row.length > 5) {
+        let nuclideInfo = [elementName];
+          for(var k= 1; k < 5; k++) {
+            let data = row[k].innerHTML
+            if(k == 3 || k == 4) {
+              data = getRidOfCertainChar(data)
+            }
+            if(k==4 && row[k].innerHTML.includes('Stable')) {
+              data = 'stable';
+            }
+            nuclideInfo.push(data);
+          }
+        isotopeArray.push(nuclideInfo);
+      }
+
+   }
+   console.log('Isotope arry: ', isotopeArray);
   }
-  //add the data to an array of arrays
+
+  function getRidOfCertainChar(str) {
+    //get rid of stuff between (), then get rid of non ascii characters
+    let result = str.replace(/ *\([^)]*\) */g, "").replace(/[^\x00-\x7F]/g, "");
+
+    if(result.includes('<sup>')) {
+      result = result.replace('<sup>', 'e');
+      let units = result.slice(result.indexOf('&nbsp;'), result.length)
+      result = result.slice(0, result.indexOf('</sup>')) + units;
+    }
+    result = result.replace('&nbsp;', ' ');
+    // if(result.indexOf('<br>') >= -1) {
+    //   result = result.slice(0, result.indexOf('<br>')+1);
+    // }
+    return result;
+  }
+
+  function addAdditionalColumns() {
+    isotopeArray.forEach(isotope => {
+      let massNumber = parseInt(isotope[1], 10) + parseInt(isotope[2], 10);
+      let isotopeName = isotope[0] + '-' + massNumber;
+      isotope[5] = massNumber;
+      isotope[6] = isotopeName;
+    })
+  }
 }
 
 var isotopeGrabber  = new IsotopeGrabber();
